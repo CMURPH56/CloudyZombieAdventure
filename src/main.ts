d118fc8169bb2827058ef037481184cebde31b70
cmurph56 <cammurphy195@gmail.com>
2021-08-23 19:36:02 -0500

    starting building out project included gitignore in project

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import * as Phaser from 'phaser';
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+  title: 'Cloudy Adventure Game',
+
+  type: Phaser.AUTO,
+
+  scale: {
+    width: window.innerWidth,
+    height: window.innerHeight
+  },
+
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+    },
+  },
+  parent: 'game',
+  backgroundColor: '#000000',
+};
+
+export const game = new Phaser.Game(gameConfig);
