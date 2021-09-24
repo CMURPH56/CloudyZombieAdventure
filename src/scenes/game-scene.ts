5e29dcab88389846993f8249f961e047f5efa201
cmurph56 <cammurphy195@gmail.com>
2021-09-23 22:20:27 -0500

    idk what i am doing anymore

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -5,7 +5,9 @@ export class GameScene extends Phaser.Scene {
   private map: Phaser.Tilemaps.Tilemap;
   private tileSet: Phaser.Tilemaps.Tileset;
   private cloudy: Cloudy;
-  private platform: Platform
+  private platform: Platform;
+  private backgroundLayer: Phaser.Tilemaps.TilemapLayer;
+  private foregroundLayer: Phaser.Tilemaps.TilemapLayer;
   private platforms: Phaser.GameObjects.Group;
   private meow: Phaser.Sound.BaseSound;
 
@@ -26,27 +28,56 @@ export class GameScene extends Phaser.Scene {
     
     // create our tilemap from Tiled JSON
     this.map = this.make.tilemap({key: this.registry.get('level')});
+
+
     // add our tileset and layers to our tilemap
     this.tileSet = this.map.addTilesetImage('tiles');
-    
-    this.cloudy = new Cloudy({
-      scene: this,
-      x: 50,
-      y: 100,
-      texture: 'bird'
-    });
 
-    this.platform = new Platform({
+    // tile set is returning null
+    console.log(this.tileSet);
+
+
+    this.backgroundLayer= this.map.createLayer(
+      'backgroundLayer',
+      this.tileSet,
+      0,
+      0
+    );
+
+    this.foregroundLayer = this.map.createLayer(
+      'foregroundLayer',
+      this.tileSet,
+      0,
+      0
+    );
+
+    // I dont think these should be returning null
+    // console.log(this.foregroundLayer);
+    // console.log(this.backgroundLayer);
+    // // this.foregroundLayer.setName('foregroundLayer');
+    
+    // // set collision for tiles with property collide set to true
+    // this.foregroundLayer.setCollisionByProperty({ collide:true })
+
+      
+      this.cloudy = new Cloudy({
         scene: this,
-        x:  50,
-        y: 500,
-        texture: 'Platform'
-    })
-    this.platforms = this.add.group({
-      runChildUpdate: true
-    })
-    // Colliders
-    this.physics.add.collider(this.cloudy, this.platform);
+        x: 50,
+        y: 100,
+        texture: 'bird'
+      });
+
+      this.platform = new Platform({
+          scene: this,
+          x:  50,
+          y: 500,
+          texture: 'Platform'
+      })
+      this.platforms = this.add.group({
+        runChildUpdate: true
+      })
+      // Colliders
+      this.physics.add.collider(this.cloudy, this.platform);
   }
 
   public update() {
