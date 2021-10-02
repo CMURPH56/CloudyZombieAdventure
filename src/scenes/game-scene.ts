10574008d50040e8a9d62bfa32a7179c75e8ddd0
cmurph56 <cammurphy195@gmail.com>
2021-10-01 20:58:34 -0500

    commented out some code for testing

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -27,40 +27,13 @@ export class GameScene extends Phaser.Scene {
     // SETUP TILE MAP
     
     // create our tilemap from Tiled JSON
-    this.map = this.make.tilemap({key: this.registry.get('level')});
+    this.map = this.make.tilemap({key: 'level1'});
 
+    console.log(this.map)
 
-    // add our tileset and layers to our tilemap
-    this.tileSet = this.map.addTilesetImage('tiles');
+    // this.loadObjectsFromMap();
 
-    // tile set is returning null
-    console.log(this.tileSet);
-
-
-    this.backgroundLayer= this.map.createLayer(
-      'backgroundLayer',
-      this.tileSet,
-      0,
-      0
-    );
-
-    this.foregroundLayer = this.map.createLayer(
-      'foregroundLayer',
-      this.tileSet,
-      0,
-      0
-    );
-
-    // I dont think these should be returning null
-    // console.log(this.foregroundLayer);
-    // console.log(this.backgroundLayer);
-    // // this.foregroundLayer.setName('foregroundLayer');
-    
-    // // set collision for tiles with property collide set to true
-    // this.foregroundLayer.setCollisionByProperty({ collide:true })
-
-      
-      this.cloudy = new Cloudy({
+    this.cloudy = new Cloudy({
         scene: this,
         x: 50,
         y: 100,
@@ -82,5 +55,12 @@ export class GameScene extends Phaser.Scene {
 
   public update() {
     this.cloudy.update();
+  }
+
+
+  private loadObjectsFromMap(): void {
+    // get the object layer in the tilemap named 'objects'
+    const objects = this.map.getObjectLayer('objects').objects as any[];
+    console.log(objects);
   }
 }
